06c94225be372c3cb9d8512f47c31a8c0054239b
isaac6 <isaac.hall@colostate.edu>
2018-03-03 20:14:52 -0700

    Added North Dakota controller to the fuel framework. Only has index/login page right now.

---
+++ classes/controller/northdakota.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.8
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2016 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_NorthDakota extends Controller
+{
+	/**
+	 * The basic welcome message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		return Response::forge(View::forge('northdakota/index'));
+	}	
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(Presenter::forge('northdakota/404'), 404);
+	}
+}
---
+++ views/northdakota/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title> North Dakota P1 </title>
+<link rel= "stylesheet" href="style.css">
+<meta name= "author" content= "Isaac Hall, Zachary Rule">
+<meta name= "description" content= "This site is part of a CSU CT 310 Course Project.">
+</head>
+<body>
+
+<?php 
+if (!(isset($_POST['username']) || isset ($_POST['password']))) {?>
+	<form action="login.php" method="POST">
+		<input type="text" name="username" placeholder="Please enter username"/>
+		<input type="password" name="password" placeholder="Please enter password"/>
+		<input type="submit" value="submit">
+	</form>
+<?php 
+}
+else {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	if(($username === 'ct310') && (md5($password) === 'a6cebbf02cc311177c569525a0f119d7')){
+		session_start();
+		$_SESSION['username'] = 'ct310';
+		$_SESSION['id'] = 12345;
+		header('Location: loggedin.php');
+	}elseif(($username === 'zachrule') && (md5($password) === '3bc5be6ae2353708a6612962db3e587b')){
+                session_start();
+                $_SESSION['username'] = 'ct310';
+                $_SESSION['id'] = 123456;
+                header('Location: loggedin.php');
+        }
+// hardcode username and password here
+	else{
+		echo 'Invalid Credentials';
+	}
+}
+?>
+	
+</body>
+<h1> North Dakota </h1>
+<nav>
+<a href="">About Us</a><p>|</p>
+<a href="">Theodore Roosevelt National Park</a><p>|</p>
+<a href="">International Peace Garden</a><p>|</p>
+<a href="">Maah Daah Hey Trail</a><p>|</p>
+</nav>
+</html>
