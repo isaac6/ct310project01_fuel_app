2fc9767de2f2bad6f38f8bd4dd9d56ddf81fcae6
isaac6 <isaac.hall@colostate.edu>
2018-03-06 21:24:16 -0700

    added all pages and edited navigation

---
+++ views/northdakota/attraction2.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+	<title>North Dakota | International Peace Garden</title>
+	<meta charset="utf-8" />
+	<?php echo Asset::css('northdakota.css'); ?>
+	<meta name= "author" content= "Isaac Hall, Zachary Rule">
+	<meta name= "description" content= "This site is part of a CSU CT 310 Course Project.">
+</head>
+
+<body>
+	<?php echo $header; ?>
+
+    <?php echo $navigation; ?>
+
+    <div id="content">
+
+	    <h1>International Peace Garden</h1>
+	    <p>Main page content here</p>
+
+    </div>
+
+	<?php echo $footer; ?>
+
+</body>
+</html>
--- views/northdakota/navigation.php
+++ views/northdakota/navigation.php
@@ -1,9 +1,9 @@
 <div id="navigation">
     <ul>
         <li><a href="http://www.cs.colostate.edu/~isaach/ct310/index.php/northdakota">Home</a></li>
-        <li><a href="#">Theodore Roosevelt National Park</a></li>
-        <li><a href="#">International Peace Garden</a></li>
-        <li><a href="#">Maah Daah Hey Trail</a></li>
+        <li><a href="http://www.cs.colostate.edu/~isaach/ct310/index.php/northdakota/attraction1">Theodore Roosevelt National Park</a></li>
+        <li><a href="http://www.cs.colostate.edu/~isaach/ct310/index.php/northdakota/attraction2">International Peace Garden</a></li>
+        <li><a href="http://www.cs.colostate.edu/~isaach/ct310/index.php/northdakota/attraction3">Maah Daah Hey Trail</a></li>
         <li><a href="http://www.cs.colostate.edu/~isaach/ct310/index.php/northdakota/aboutus">About Us</a></li>
     </ul>
 </div>
