b3719486b411705531574042349d4cc0e1ff8c30
isaac6 <isaac.hall@colostate.edu>
2018-03-06 23:39:05 -0700

    authentication:

--- views/northdakota/index.php
+++ views/northdakota/index.php
@@ -15,15 +15,19 @@
 
     <div id="content">
 
-	    <h1>Home</h1>
-	    <p>North Dakota is a state in the midwestern and
-			 northern regions of the United States. It is
-			 the nineteenth largest in area, the fourth
-			 smallest by population, and the fourth most
-			 sparsely populated of the 50 states. North Dakota
-			 was admitted as the 39th state to the Union on
-			 November 2, 1889. Its capital is Bismarck, and
-			 its largest city is Fargo.</p>
+		<?php echo $authentication; ?>
+
+		<div id="main">
+		    <h1>Home</h1>
+		    <p>North Dakota is a state in the midwestern and
+				 northern regions of the United States. It is
+				 the nineteenth largest in area, the fourth
+				 smallest by population, and the fourth most
+				 sparsely populated of the 50 states. North Dakota
+				 was admitted as the 39th state to the Union on
+				 November 2, 1889. Its capital is Bismarck, and
+				 its largest city is Fargo.</p>
+		</div>
 
     </div>
 
