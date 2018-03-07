157a8dfcf4b6c80d3191327fcb1b2fcbfa3a11e3
isaac6 <isaac.hall@colostate.edu>
2018-03-06 22:44:25 -0700

    Removed navigation.php and added it to .gitignore

--- views/northdakota/navigation.php
+++
@@ -1,9 +0,0 @@
-<div id="navigation">
-    <ul>
-        <li><a href="http://www.cs.colostate.edu/~zachrule/ct310/index.php/northdakota">Home</a></li>
-        <li><a href="http://www.cs.colostate.edu/~zachrule/ct310/index.php/northdakota/attraction1">Theodore Roosevelt National Park</a></li>
-        <li><a href="http://www.cs.colostate.edu/~zachrule/ct310/index.php/northdakota/attraction2">International Peace Garden</a></li>
-        <li><a href="http://www.cs.colostate.edu/~zachrule/ct310/index.php/northdakota/attraction3">Maah Daah Hey Trail</a></li>
-        <li><a href="http://www.cs.colostate.edu/~zachrule/ct310/index.php/northdakota/aboutus">About Us</a></li>
-    </ul>
-</div>
