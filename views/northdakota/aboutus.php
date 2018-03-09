06cd156d5dbcb8301d1a7b0b0a740e2bbda8372a
Zachary Rule <zachrule@phoenix.cs.colostate.edu>
2018-03-08 23:08:00 -0700

    authentication done in the controller. your login info should work. started using authentication boolean to create comment box on each attraction

--- views/northdakota/aboutus.php
+++ views/northdakota/aboutus.php
@@ -14,8 +14,9 @@
     <?php echo $navigation; ?>
 
     <div id="ABcontent">
-
 	    <h1>About Us</h1>
+<?php echo '<a href="http://www.mappery.com/map-of/north-dakota-map-2"><img src="http://www.cs.colostate.edu/~zachrule/ct310/assets/img/ndmap.jpg"/></a> ' ?>
+
 	    <p>ORIGIN OF STATE NAME: The state was formerly the northern section of Dakota Territory; dakota is a Siouan word meaning "allies."</p>
       <p>NICKNAME: Peace Garden State; Flickertail State.</p>
       <p>CAPITAL: Bismarck.</p>
--- views/northdakota/attraction1.php
+++ views/northdakota/attraction1.php
@@ -21,18 +21,20 @@
 	    
 	    <p>Theodore Roosevelt National Park is a United States National Park comprising three geographically separated areas of badlands in western North Dakota. The park was named for U.S. President Theodore Roosevelt. The park covers 70,446 acres of land in three sections: the North Unit, the South Unit, and the Elkhorn Ranch Unit.
 
-The park's larger South Unit lies alongside Interstate 94 near Medora, North Dakota. The smaller North Unit is situated about 80 mi (130 km) north of the South Unit, on U.S. Highway 85, just south of Watford City, North Dakota. Roosevelt's Elkhorn Ranch is located between the North and South units, approximately 20 mi (32 km) west of US 85 and Fairfield, North Dakota. The Little Missouri River flows through all three units of the park. The Maah Daah Hey Trail connects all three units.</p>
-	    <h2>Comments</h2>
-	    <?php
-
-
-            if(!isset($_SESSION['username']))
-            echo "You must login to view this page. Login <a href='index.php'>here</a>";
-            else {
-            }
-            ?>
-    </div>
-
+The park's larger South Unit lies alongside Interstate 94 near Medora, North Dakota. The smaller North Unit is situated about 80 mi (130 km) north of the South Unit, on U.S. Highway 85, just south of Watford City, North Dakota. Roosevelt's Elkhorn Ranch is located between the North and South units, approximately 20 mi (32 km) west of US 85 and Fairfield, North Dakota. The Little Missouri River flows through all three units of the park. The Maah Daah Hey Trail connects all three units.</p>   
+      <h2>Comments</h2>
+      <?php $session = Session::instance();
+            $authentication = $session->get('authentication');
+      ?>
+      <?php if ($authentication==TRUE): ?>
+        <form action="#" method="post">
+        Type to comment: <input type="text" name="usercomment">
+        <input type="submit" value="Submit" name="submit">
+        </form>
+      <?php elseif ($authentication==FALSE): ?>
+        You must login to comment on this page. Login <a href='index.php'>here</a>
+      <?php endif; ?>
+      
 	<?php echo $footer; ?>
 
 </body>
