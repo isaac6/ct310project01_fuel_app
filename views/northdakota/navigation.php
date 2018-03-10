d7369a78ec42d0aecf7d5e5c24a0b293c6ce14cf
isaac6 <isaac.hall@colostate.edu>
2018-03-09 17:06:01 -0700

    Updated navigation for better usage on devices with small screens

--- views/northdakota/navigation.php
+++ views/northdakota/navigation.php
@@ -2,9 +2,9 @@
     <ul>
         <li><a href="<?php echo Uri::create('index.php/northdakota'); ?>">Home</a></li>
         <li><a href="<?php echo Uri::create('index.php/northdakota/login'); ?>">Login</a></li>
-        <li><a href="<?php echo Uri::create('index.php/northdakota/attraction1'); ?>">Theodore Roosevelt National Park</a></li>
-        <li><a href="<?php echo Uri::create('index.php/northdakota/attraction2'); ?>">International Peace Garden</a></li>
-        <li><a href="<?php echo Uri::create('index.php/northdakota/attraction3'); ?>">Maah Daah Hey Trail</a></li>
+        <li><a href="<?php echo Uri::create('index.php/northdakota/attraction1'); ?>">Theodore Roosevelt<br/>National Park</a></li>
+        <li><a href="<?php echo Uri::create('index.php/northdakota/attraction2'); ?>">International<br/>Peace Garden</a></li>
+        <li><a href="<?php echo Uri::create('index.php/northdakota/attraction3'); ?>">Maah Daah<br/>Hey Trail</a></li>
         <li><a href="<?php echo Uri::create('index.php/northdakota/aboutus'); ?>">About Us</a></li>
     </ul>
 </div>
